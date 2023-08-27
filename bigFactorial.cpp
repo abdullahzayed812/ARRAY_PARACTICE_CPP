bfc2cce74281b61775c31317f28142b36dbe7b93
Abdullah_Zayed <abdullah@quadratechsoft.com>
2023-08-28 01:13:18 +0300

    add bigFactorial problem like 100

---
+++ bigFactorial.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_SIZE 500
+
+void factorial(int n);
+int multiply(int currentIndex, int arr[], int arrSize);
+
+void factorial(int n)
+{
+  int result[MAX_SIZE];
+  result[0] = 1;
+  int resultSize = 1;
+
+  for (int i = 2; i <= n; i++)
+    resultSize = multiply(i, result, resultSize);
+
+  for (int i = resultSize - 1; i >= 0; i--)
+    cout << result[i];
+
+  cout << "\n"
+       << resultSize;
+}
+
+int multiply(int currentIndex, int arr[], int arrSize)
+{
+  long carry = 0;
+
+  for (int i = 0; i < arrSize; i++)
+  {
+    long currentArrItem = arr[i] * currentIndex + carry;
+
+    arr[i] = currentArrItem % 10;
+
+    carry = currentArrItem / 10;
+  }
+
+  while (carry)
+  {
+    arr[currentIndex++] = carry % 10;
+    carry = carry / 10;
+  }
+
+  return currentIndex;
+}
+
+int main()
+{
+  factorial(20);
+
+  return 0;
+}
